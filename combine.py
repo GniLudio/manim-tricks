062768fc6085d18050d48cec084f5191553949a6
GniLudio <50866361+GniLudio@users.noreply.github.com>
2026-06-17 17:13:57 +0200

    Add combine utility

---
+++ combine.py
@@ -0,0 +1,16 @@
+from manim import AnimationGroup, Scene
+
+
+class Combine(AnimationGroup):
+    def _setup_scene(self, scene: Scene) -> None:
+        super()._setup_scene(scene)
+        self._scene = scene
+        self._initial_mobjects = {mob: mob.copy() for mob in self.group}
+
+    def interpolate(self, alpha: float) -> None:
+        for mobject, initial_mobject in self._initial_mobjects.items():
+            mobject.become(initial_mobject)
+        for animation in self.animations:
+            animation._setup_scene(self._scene)
+            animation.begin()
+            animation.interpolate(alpha)
